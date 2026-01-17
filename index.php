0232cd9eaaecd182df806dbdef02bce00451217b
mikeycodingstuff <108337844+mikeycodingstuff@users.noreply.github.com>
2026-01-17 23:07:28 +0000

    pint config

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 require 'vendor/autoload.php';
 
 use Rpg\Player;
 
 $name = readline('Enter your name: ');
-$age = readline("Age: ");
+$age = (int) readline('Age: ');
 $weapon = readline('Weapon: ');
 
 $player = new Player($name, $age, $weapon);
 
-echo(
+echo
     "Your name is {$player->getName()}. \n" .
     "Your age is {$player->getAge()}. \n" .
-    "Your weapon is {$player->getWeapon()}. \n"
-);
+    "Your weapon is {$player->getWeapon()}. \n";
