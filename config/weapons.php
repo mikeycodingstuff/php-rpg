51b95bde9cdcb3b8a6fc5c5fb29a9ac85eb5ca00
mikeycodingstuff <108337844+mikeycodingstuff@users.noreply.github.com>
2026-01-18 18:05:11 +0000

    weapons config

---
+++ config/weapons.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'sword' => [
+        'emoji' => '⚔️',
+        'description' => 'A balanced blade for versatile combat',
+        'damage' => 20,
+    ],
+    'axe' => [
+        'emoji' => '🪓',
+        'description' => 'A heavy weapon that crushes enemies with brutal force',
+        'damage' => 25,
+    ],
+    'dagger' => [
+        'emoji' => '🗡️',
+        'description' => 'A swift blade for quick, precise strikes',
+        'damage' => 15,
+    ],
+];
