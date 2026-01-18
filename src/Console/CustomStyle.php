4f13ddfc3e90a2884609ddc996a788ff91347f66
mikeycodingstuff <108337844+mikeycodingstuff@users.noreply.github.com>
2026-01-18 18:03:58 +0000

    override info

--- src/Console/CustomStyle.php
+++ src/Console/CustomStyle.php
@@ -8,6 +8,11 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 
 class CustomStyle extends SymfonyStyle
 {
+    public function info(string|array $message): void
+    {
+        $this->block($message, null, 'fg=green', ' ', true);
+    }
+
     public function success(string|array $message): void
     {
         $this->block($message, null, 'fg=black;bg=green', ' ', true);
