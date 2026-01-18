c983128d7e535b2626d4375e3786c2e2514eb198
mikeycodingstuff <108337844+mikeycodingstuff@users.noreply.github.com>
2026-01-18 18:05:54 +0000

    refactor to a table

--- src/Console/Prompts/GamePrompts.php
+++ src/Console/Prompts/GamePrompts.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace Rpg\Console\Prompts;
 
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Str;
 use Rpg\Console\CustomStyle;
 use RuntimeException;
+use Symfony\Component\Console\Helper\TableStyle;
 
 class GamePrompts
 {
@@ -15,7 +18,7 @@ class GamePrompts
     public function askName(): string
     {
         return $this->io->ask(
-            question: 'Enter your name: ',
+            question: 'Enter your name',
             validator: function ($answer) {
                 if (Str::of($answer)->trim()->length() < 2) {
                     throw new RuntimeException('Name must be at least 2 characters');
@@ -28,15 +31,38 @@ class GamePrompts
 
     public function chooseWeapon(): string
     {
-        return $this->io->choice(
-            question: 'Choose your weapon:',
-            choices: [
-                '⚔️  Sword (balanced damage)',
-                '🪓 Axe (heavy damage)',
-                '🗡️  Dagger (quick strikes)',
-            ],
-            default: 0,
+        $table = $this->io->createTable();
+
+        $tableStyle = new TableStyle;
+
+        $tableStyle
+            ->setHorizontalBorderChars('-')
+            ->setVerticalBorderChars('|');
+
+        $table->setStyle($tableStyle);
+
+        $weapons = Collection::make(Config::get('weapons'));
+
+        $weapons->each(function (array $stats, string $name) use ($table) {
+            $weapon = Str::title($name);
+            $table->addRow(
+                [
+                    "<fg=bright-cyan>$weapon</>",
+                    "<fg=yellow>{$stats['damage']}</>",
+                    "<fg=gray>{$stats['description']}</>",
+                ]);
+        });
+
+        $table->render();
+
+        $this->io->writeln('');
+
+        $choice = $this->io->choice(
+            question: 'Choose your weapon',
+            choices: $weapons->keys()->map(fn ($key) => Str::title($key))->all(),
         );
+
+        return Str::lower($choice);
     }
 
     public function confirmReady(): bool
